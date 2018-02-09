f9626b7ab1c9027c4b0146c38cd6f70252ead991
Marco Kellershoff <marco.kellershoff@gmail.com>
2018-02-09 16:34:40 +0100

    Adds `.gitignore` to `dist`
    This commit removes all files from the `dist` directory
    and puts `.gitignore` file inside the directory which will prevent all
    files inside the `dist` directory to get inside the repo again.

--- dist/container.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-import * as React from "react";
-export interface ContainerProps {
-    children: React.ReactElement<any>;
-}
-declare const _default: React.ComponentClass<{}>;
-export default _default;
